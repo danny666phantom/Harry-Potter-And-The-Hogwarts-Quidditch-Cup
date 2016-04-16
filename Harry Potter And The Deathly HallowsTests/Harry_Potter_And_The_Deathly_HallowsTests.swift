72212cd48d57d27713dd4ad431583d0587a140d7
danny666phantom <danny_phantom_@rven.local>
2016-04-15 20:22:44 -0600

    Initial Commit

---
+++ Harry Potter And The Deathly HallowsTests/Harry_Potter_And_The_Deathly_HallowsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Harry_Potter_And_The_Deathly_HallowsTests.swift
+//  Harry Potter And The Deathly HallowsTests
+//
+//  Created by [ r∆ven ] on 4/15/16.
+//  Copyright © 2016 Salt Lake Community College. All rights reserved.
+//
+
+import XCTest
+@testable import Harry_Potter_And_The_Deathly_Hallows
+
+class Harry_Potter_And_The_Deathly_HallowsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
